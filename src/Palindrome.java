923043d376b4ef71095a7530047da070b6b5873c
Peter Veres2 <Peter_Veres2@epam.com>
2016-03-18 15:28:13 +0100

    tasks

---
+++ src/Palindrome.java
@@ -0,0 +1,13 @@
+/**
+ * Definition: A palindrome is a string that reads the same forward and 
+ * backward.
+ * For example, "abcba" is a palindrome, "abab" is not.
+ * Please implement this method to
+ * return true if the parameter is a palindrome and false otherwise. 
+ * 
+ * @author peter_veres2
+ *
+ */
+public class Palindrome {
+
+}
---
+++ src/WordCount.java
@@ -0,0 +1,11 @@
+/**
+ * Please implement this method to return the word count 
+ * in a given String. Assume that the parameter String 
+ * can only contain spaces and alphanumeric characters.
+ * @author peter_veres2
+ *
+ */
+public class WordCount {
+
+	
+}
