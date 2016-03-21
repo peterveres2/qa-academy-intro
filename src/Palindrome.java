422929a6a34c8a66470e38921048ea927644ae3d
Peter Veres2 <Peter_Veres2@epam.com>
2016-03-21 09:07:29 +0100

    day2 tasks

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,13 +1,50 @@
 /**
- * Definition: A palindrome is a string that reads the same forward and 
- * backward.
- * For example, "abcba" is a palindrome, "abab" is not.
- * Please implement this method to
- * return true if the parameter is a palindrome and false otherwise. 
+ * Definition: A palindrome is a string that reads the same forward and
+ * backward. For example, "abcba" is a palindrome, "abab" is not. Please
+ * implement this method to return true if the parameter is a palindrome and
+ * false otherwise.
  * 
  * @author peter_veres2
  *
  */
 public class Palindrome {
+
+	public static void main(String[] args) {
+
+		String textToCheck = "Not";
+		System.out.println(textToCheck + " : " + isPalindrome(textToCheck));
+
+		textToCheck = "NoN";
+		System.out.println(textToCheck + " : " + isPalindrome(textToCheck));
+
+		textToCheck = "Non";
+		System.out.println(textToCheck + " : " + isPalindrome(textToCheck));
+
+		textToCheck = "abcba";
+		System.out.println(textToCheck + " : " + isPalindrome(textToCheck));
+
+		textToCheck = "abab";
+		System.out.println(textToCheck + " : " + isPalindrome(textToCheck));
+
+		textToCheck = " ";
+		System.out.println(textToCheck + " : " + isPalindrome(textToCheck));
+
+		textToCheck = "";
+		System.out.println(textToCheck + " : " + isPalindrome(textToCheck));
+
+	}
+
+	static boolean isPalindrome(String str) {
+		if (str.length() == 0) {
+			return true;
+		}
+		int lastIndex = str.length() - 1;
+		for (int i = 0; i <= (str.length()) / 2; i++) {
+			if (str.charAt(i) != str.charAt(lastIndex - i)) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
--- src/WordCount.java
+++ src/WordCount.java
@@ -1,11 +1,46 @@
 /**
- * Please implement this method to return the word count 
- * in a given String. Assume that the parameter String 
- * can only contain spaces and alphanumeric characters.
+ * Please implement this method to return the word count in a given String.
+ * Assume that the parameter String can only contain spaces and alphanumeric
+ * characters.
+ * 
  * @author peter_veres2
  *
  */
 public class WordCount {
 
-	
+	public static void main(String[] args) {
+		String textToCheck = "Not allowed operation";
+		System.out.println(wordCount(textToCheck));
+
+		textToCheck = " Not allowed  operation";
+		System.out.println(wordCount(textToCheck));
+
+		textToCheck = " Not    allowed  operation 685";
+		System.out.println(wordCount(textToCheck));
+
+		textToCheck = " Not 4   allowed  ew operation 685";
+		System.out.println(wordCount(textToCheck));
+
+		textToCheck = "";
+		System.out.println(wordCount(textToCheck));
+
+		textToCheck = "   ";
+		System.out.println(wordCount(textToCheck));
+
+	}
+
+	static int wordCount(String str) {
+		int wordCount = 0;
+		char prevChar = ' ';
+		char actualChar;
+		for (int i = 0; i < str.length(); i++) {
+			actualChar = str.charAt(i);
+			if ((actualChar != ' ') && (prevChar == ' ')) {
+				wordCount++;
+			}
+			prevChar = actualChar;
+		}
+		return wordCount;
+	}
+
 }
